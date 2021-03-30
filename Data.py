360f4915a82f936977c766a48d001632468c9f27
savoiadiego <diego.savoia@mail.polimi.it>
2021-03-30 09:53:43 +0200

    Integrated form data into Simulator

---
+++ Data.py
@@ -0,0 +1,143 @@
+import pandas as pd
+
+
+class Data:
+    def __init__(self):
+        # Importing the csv containing the form results
+        data = pd.read_csv('DIAForm.csv')
+        data.columns = ['Date', 'Age', 'Sex', 'AW', 'WS0', 'WS10', 'WS20', 'WS50']
+
+        # Total number of people who submitted the form
+        n = len(data.Date)
+
+        # Classes of customers
+        c1 = data[(data.Sex == 'Femmina') & (data.Age <= 35)]               # C1: Female under 35
+        c2 = data[(data.Sex == 'Maschio') & (data.Age <= 35)]               # C2: Male under 35
+        c3 = data[(data.Sex == 'Femmina') & (data.Age > 35)]                # C3: Female over 35
+        c4 = data[(data.Sex == 'Maschio') & (data.Age > 35)]                # C4: Male over 35
+
+        # Total number of customers per class
+        self.n1 = len(c1.Date)
+        self.n2 = len(c2.Date)
+        self.n3 = len(c3.Date)
+        self.n4 = len(c4.Date)
+
+        # Number of people per class that buy item 1
+        nc1_i1 = c1.Date[c1.AW == 'Sì'].count()
+        nc2_i1 = c2.Date[c2.AW == 'Sì'].count()
+        nc3_i1 = c3.Date[c3.AW == 'Sì'].count()
+        nc4_i1 = c4.Date[c4.AW == 'Sì'].count()
+
+        # Probability (per class) that a customer buys item 1
+        self.c1_i1_param = nc1_i1 / self.n1
+        self.c2_i1_param = nc2_i1 / self.n2
+        self.c3_i1_param = nc3_i1 / self.n3
+        self.c4_i1_param = nc4_i1 / self.n4
+
+        # Number of people per class that buy item 2 given item 1 + P0
+        nc1_i2_p0 = c1.Date[c1.WS0 == 'Sì'].count()
+        nc2_i2_p0 = c2.Date[c2.WS0 == 'Sì'].count()
+        nc3_i2_p0 = c3.Date[c3.WS0 == 'Sì'].count()
+        nc4_i2_p0 = c4.Date[c4.WS0 == 'Sì'].count()
+
+        # Probability (per class) that a customer buys item 2 given item 1 + P0
+        self.c1_i21_p0_param = nc1_i2_p0 / self.n1
+        self.c2_i21_p0_param = nc2_i2_p0 / self.n2
+        self.c3_i21_p0_param = nc3_i2_p0 / self.n3
+        self.c4_i21_p0_param = nc4_i2_p0 / self.n4
+
+        # Number of people per class that buy item 2 given item 1 + P1
+        nc1_i2_p1 = c1.Date[c1.WS10 == 'Sì'].count()
+        nc2_i2_p1 = c2.Date[c2.WS10 == 'Sì'].count()
+        nc3_i2_p1 = c3.Date[c3.WS10 == 'Sì'].count()
+        nc4_i2_p1 = c4.Date[c4.WS10 == 'Sì'].count()
+
+        # Probability (per class) that a customer buys item 2 given item 1 + P1
+        self.c1_i21_p1_param = (nc1_i2_p1 + nc1_i2_p0) / self.n1
+        self.c2_i21_p1_param = (nc2_i2_p1 + nc2_i2_p0) / self.n2
+        self.c3_i21_p1_param = (nc3_i2_p1 + nc3_i2_p0) / self.n3
+        self.c4_i21_p1_param = (nc4_i2_p1 + nc4_i2_p0) / self.n4
+
+        # Number of people per class that buy item 2 given item 1 + P2
+        nc1_i2_p2 = c1.Date[c1.WS20 == 'Sì'].count()
+        nc2_i2_p2 = c2.Date[c2.WS20 == 'Sì'].count()
+        nc3_i2_p2 = c3.Date[c3.WS20 == 'Sì'].count()
+        nc4_i2_p2 = c4.Date[c4.WS20 == 'Sì'].count()
+
+        # Probability (per class) that a customer buys item 2 given item 1 + P2
+        self.c1_i21_p2_param = (nc1_i2_p2 + nc1_i2_p1 + nc1_i2_p0) / self.n1
+        self.c2_i21_p2_param = (nc2_i2_p2 + nc2_i2_p1 + nc2_i2_p0) / self.n2
+        self.c3_i21_p2_param = (nc3_i2_p2 + nc3_i2_p1 + nc3_i2_p0) / self.n3
+        self.c4_i21_p2_param = (nc4_i2_p2 + nc4_i2_p1 + nc4_i2_p0) / self.n4
+
+        # Number of people per class that buy item 2 given item 1 + P3
+        nc1_i2_p3 = c1.Date[c1.WS50 == 'Sì'].count()
+        nc2_i2_p3 = c2.Date[c2.WS50 == 'Sì'].count()
+        nc3_i2_p3 = c3.Date[c3.WS50 == 'Sì'].count()
+        nc4_i2_p3 = c4.Date[c4.WS50 == 'Sì'].count()
+
+        # Probability (per class) that a customer buys item 2 given item 1 + P3
+        self.c1_i21_p3_param = (nc1_i2_p3 + nc1_i2_p2 + nc1_i2_p1 + nc1_i2_p0) / self.n1
+        self.c2_i21_p3_param = (nc2_i2_p3 + nc2_i2_p2 + nc2_i2_p1 + nc2_i2_p0) / self.n2
+        self.c3_i21_p3_param = (nc3_i2_p3 + nc3_i2_p2 + nc3_i2_p1 + nc3_i2_p0) / self.n3
+        self.c4_i21_p3_param = (nc4_i2_p3 + nc4_i2_p2 + nc4_i2_p1 + nc4_i2_p0) / self.n4
+
+    def get_n(self, n):
+        if n == 1:
+            return self.n1
+        elif n == 2:
+            return self.n2
+        elif n == 3:
+            return self.n3
+        elif n == 4:
+            return self.n4
+
+    def get_i1_param(self, n):
+        if n == 1:
+            return self.c1_i1_param
+        elif n == 2:
+            return self.c2_i1_param
+        elif n == 3:
+            return self.c3_i1_param
+        elif n == 4:
+            return self.c4_i1_param
+
+    def get_i21_p0_param(self, n):
+        if n == 1:
+            return self.c1_i21_p0_param
+        elif n == 2:
+            return self.c2_i21_p0_param
+        elif n == 3:
+            return self.c3_i21_p0_param
+        elif n == 4:
+            return self.c4_i21_p0_param
+
+    def get_i21_p1_param(self, n):
+        if n == 1:
+            return self.c1_i21_p1_param
+        elif n == 2:
+            return self.c2_i21_p1_param
+        elif n == 3:
+            return self.c3_i21_p1_param
+        elif n == 4:
+            return self.c4_i21_p1_param
+
+    def get_i21_p2_param(self, n):
+        if n == 1:
+            return self.c1_i21_p2_param
+        elif n == 2:
+            return self.c2_i21_p2_param
+        elif n == 3:
+            return self.c3_i21_p2_param
+        elif n == 4:
+            return self.c4_i21_p2_param
+
+    def get_i21_p3_param(self, n):
+        if n == 1:
+            return self.c1_i21_p3_param
+        elif n == 2:
+            return self.c2_i21_p3_param
+        elif n == 3:
+            return self.c3_i21_p3_param
+        elif n == 4:
+            return self.c4_i21_p3_param
--- Simulator.py
+++ Simulator.py
@@ -2,6 +2,7 @@ import numpy as np
 from Day import *
 from Group import *
 from CustomerData import *
+from Data import *
 
 
 class Simulator:
@@ -15,16 +16,18 @@ class Simulator:
     # TODO: import data from Google module
 
     def simulation_step_1(self):
+        data = Data()
+
         p0_frac = 0.7
         p1_frac = 0.2
         p2_frac = 0.07
         p3_frac = 0.03
 
-        # Number of customers per class = potentially different Gaussian distributions
-        c1_daily = int(np.random.normal(76, 12))                # Class 1: 76 customers, sigma 12
-        c2_daily = int(np.random.normal(133, 14))               # Class 2: 133 customers, sigma 14
-        c3_daily = int(np.random.normal(107, 16))               # Class 3: 107 customers, sigma 16
-        c4_daily = int(np.random.normal(93, 17))                # Class 4: 93 customers, sigma 17
+        # Number of customers per class = potentially different Gaussian distributions TODO: are sigmas correct?
+        c1_daily = int(np.random.normal(data.get_n(1), 12))
+        c2_daily = int(np.random.normal(data.get_n(2), 14))
+        c3_daily = int(np.random.normal(data.get_n(3), 16))
+        c4_daily = int(np.random.normal(data.get_n(4), 17))
 
         p0_num = int((c1_daily + c2_daily + c3_daily + c4_daily) * p0_frac)
         p1_num = int((c1_daily + c2_daily + c3_daily + c4_daily) * p1_frac)
@@ -32,43 +35,42 @@ class Simulator:
         p3_num = int((c1_daily + c2_daily + c3_daily + c4_daily) * p3_frac)
 
         # Probability that a customer of a class buys the first item alone = Binomial
-        c1_i1 = np.random.binomial(c1_daily, 0.632) / c1_daily              # Class 1: N = 76, p = 0.632
-        c2_i1 = np.random.binomial(c2_daily, 0.114) / c2_daily            # Class 2: N = 133, p = 0.114
-        c3_i1 = np.random.binomial(c3_daily, 0.333) / c3_daily            # Class 3: N = 107, p = 0.333
-        c4_i1 = np.random.binomial(c4_daily, 0.713) / c4_daily              # Class 4: N = 93, p = 0.713
+        c1_i1 = np.random.binomial(c1_daily, data.get_i1_param(1)) / c1_daily
+        c2_i1 = np.random.binomial(c2_daily, data.get_i1_param(2)) / c2_daily
+        c3_i1 = np.random.binomial(c3_daily, data.get_i1_param(3)) / c3_daily
+        c4_i1 = np.random.binomial(c4_daily, data.get_i1_param(4)) / c4_daily
 
-        # Probability that a customer of a class buys the second item alone = Binomial
+        # Probability that a customer of a class buys the second item alone = Binomial TODO: values by hand are ok?
         c1_i2 = np.random.binomial(c1_daily, 0.276) / c1_daily              # Class 1: N = 76, p = 0.276
         c2_i2 = np.random.binomial(c2_daily, 0.421) / c2_daily            # Class 2: N = 133, p = 0.421
         c3_i2 = np.random.binomial(c3_daily, 0.358) / c3_daily            # Class 3: N = 107, p = 0.358
         c4_i2 = np.random.binomial(c4_daily, 0.452) / c4_daily              # Class 4: N = 93, p = 0.452
 
         # Probability that a customer of a class buys the second item given the first + P0 = Binomial
-        c1_i21_p0 = np.random.binomial(c1_daily, 0.512) / c1_daily           # Class 1: N = 76, p = 0.512
-        c2_i21_p0 = np.random.binomial(c2_daily, 0.830) / c2_daily         # Class 2: N = 133, p = 0.830
-        c3_i21_p0 = np.random.binomial(c3_daily, 0.122) / c3_daily         # Class 3: N = 107, p = 0.122
-        c4_i21_p0 = np.random.binomial(c4_daily, 0.010) / c4_daily           # Class 4: N = 93, p = 0.010
+        c1_i21_p0 = np.random.binomial(c1_daily, data.get_i21_p0_param(1)) / c1_daily
+        c2_i21_p0 = np.random.binomial(c2_daily, data.get_i21_p0_param(2)) / c2_daily
+        c3_i21_p0 = np.random.binomial(c3_daily, data.get_i21_p0_param(3)) / c3_daily
+        c4_i21_p0 = np.random.binomial(c4_daily, data.get_i21_p0_param(4)) / c4_daily
 
         # Probability that a customer of a class buys the second item given the first + P1 = Binomial
-        c1_i21_p1 = np.random.binomial(c1_daily, 0.845) / c1_daily           # Class 1: N = 76, p = 0.845
-        c2_i21_p1 = np.random.binomial(c2_daily, 0.831) / c2_daily         # Class 2: N = 133, p = 0.831
-        c3_i21_p1 = np.random.binomial(c3_daily, 0.145) / c3_daily         # Class 3: N = 107, p = 0.145
-        c4_i21_p1 = np.random.binomial(c4_daily, 0.201) / c4_daily           # Class 4: N = 93, p = 0.201
+        c1_i21_p1 = np.random.binomial(c1_daily, data.get_i21_p1_param(1)) / c1_daily
+        c2_i21_p1 = np.random.binomial(c2_daily, data.get_i21_p1_param(2)) / c2_daily
+        c3_i21_p1 = np.random.binomial(c3_daily, data.get_i21_p1_param(3)) / c3_daily
+        c4_i21_p1 = np.random.binomial(c4_daily, data.get_i21_p1_param(4)) / c4_daily
 
         # Probability that a customer of a class buys the second item given the first + P2 = Binomial
-        c1_i21_p2 = np.random.binomial(c1_daily, 0.872) / c1_daily           # Class 1: N = 76, p = 0.872
-        c2_i21_p2 = np.random.binomial(c2_daily, 0.872) / c2_daily         # Class 2: N = 133, p = 0.872
-        c3_i21_p2 = np.random.binomial(c3_daily, 0.367) / c3_daily         # Class 3: N = 107, p = 0.367
-        c4_i21_p2 = np.random.binomial(c4_daily, 0.364) / c4_daily           # Class 4: N = 93, p = 0.364
+        c1_i21_p2 = np.random.binomial(c1_daily, data.get_i21_p2_param(1)) / c1_daily
+        c2_i21_p2 = np.random.binomial(c2_daily, data.get_i21_p2_param(2)) / c2_daily
+        c3_i21_p2 = np.random.binomial(c3_daily, data.get_i21_p2_param(3)) / c3_daily
+        c4_i21_p2 = np.random.binomial(c4_daily, data.get_i21_p2_param(4)) / c4_daily
 
         # Probability that a customer of a class buys the second item given the first + P3 = Binomial
-        c1_i21_p3 = np.random.binomial(c1_daily, 0.910) / c1_daily           # Class 1: N = 76, p = 0.910
-        c2_i21_p3 = np.random.binomial(c2_daily, 0.700) / c2_daily         # Class 2: N = 133, p = 0.700
-        c3_i21_p3 = np.random.binomial(c3_daily, 0.662) / c3_daily         # Class 3: N = 107, p = 0.662
-        c4_i21_p3 = np.random.binomial(c4_daily, 0.546) / c4_daily           # Class 4: N = 93, p = 0.546
+        c1_i21_p3 = np.random.binomial(c1_daily, data.get_i21_p3_param(1)) / c1_daily
+        c2_i21_p3 = np.random.binomial(c2_daily, data.get_i21_p3_param(2)) / c2_daily
+        c3_i21_p3 = np.random.binomial(c3_daily, data.get_i21_p3_param(3)) / c3_daily
+        c4_i21_p3 = np.random.binomial(c4_daily, data.get_i21_p3_param(4)) / c4_daily
 
-
-        # For each class of customers, we create the different CustomerData objects according to the previous probability distributions
+        # For each class of customers, we create the different CustomerData objects according to the above distributions
         # Every time we give a promo to the customer, we decrease the number of coupons available
 
         for customer in range(c1_daily):

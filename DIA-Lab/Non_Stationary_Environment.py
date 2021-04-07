377877a396df50b60d34b415085cd439a17240c2
Francesca Pietrobon <francesca13pietrobon@gmail.com>
2021-04-07 19:14:14 +0200

    Added ChangeDetection (Lab)

--- DIA-Lab/Non_Stationary_Environment.py
+++ DIA-Lab/Non_Stationary_Environment.py
@@ -5,13 +5,12 @@ class Non_Stationary_Environment(Environment):
     def __init__(self, n_arms, probabilities, horizon):
         super().__init__(n_arms, probabilities)
         self.t = 0
-        self.horizon = horizon
+        n_phases = len(self.probabilities)
+        self.phases_size = horizon/n_phases
 
     def round(self, pulled_arm):
-        n_phases = len(self.probabilities)
-        phase_size = self.horizon / n_phases
-        current_phase = int(self.t / phase_size)
-
+        current_phase = int(self.t / self.phases_size)
         p = self.probabilities[current_phase][pulled_arm]
+        reward = np.random.binomial(1, p)
         self.t += 1
-        return np.random.binomial(1, p)
+        return reward

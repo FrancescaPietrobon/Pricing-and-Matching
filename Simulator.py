36952c2a84d5e4ea9d9d5826563c4112f54df4d1
savoiadiego <diego.savoia@mail.polimi.it>
2021-04-23 11:51:08 +0200

    Initialization for Step 5

--- Simulator.py
+++ Simulator.py
@@ -3,12 +3,14 @@ from Data import *
 from Item import *
 from LP_optimization import *
 from UCB1 import *
+from UCB_matching import *
 from TS_Learner import *
 from Environment import *
 
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import normal, binomial
+from scipy.optimize import linear_sum_assignment
 
 np.random.seed(1234)
 
@@ -183,7 +185,6 @@ class Simulator:
 
 ########################################################################################################################
 
-
     def simulation_step_4(self):
         # We choose a conversion rate for item 1 and a margin associated to 6 prices (n_arms):
         # €225 --> conversion rate: 0.65  --> margin: €75
@@ -261,3 +262,43 @@ class Simulator:
         plt.plot(np.mean(ucb1_rewards_per_experiment, axis=0), "b")
         plt.legend(["TS", "UCB1"], title="STEP 4")
         plt.show()
+
+########################################################################################################################
+
+    def simulation_step_5(self):
+
+        p = np.array([[1 / 4, 1, 1 / 4], [1 / 2, 1 / 4, 1 / 4], [1 / 4, 1 / 4, 1]])
+        opt = linear_sum_assignment(-p)
+        n_exp = 10
+        T = 3000
+        regret_ucb = np.zeros((n_exp, T))
+        for e in range(n_exp):
+            learner = UCB_Matching(p.size, *p.shape)
+            print(e)
+            rew_UCB = []
+            opt_rew = []
+            env = Environment(p.size, p)
+            for t in range(T):
+                pulled_arms = learner.pull_arm()
+                rewards = env.round(pulled_arms)
+                learner.update(pulled_arms, rewards)
+                rew_UCB.append(rewards.sum())
+                opt_rew.append(p[opt].sum())
+            regret_ucb[e, :] = np.cumsum(opt_rew) - np.cumsum(rew_UCB)
+
+        plt.figure(0)
+        plt.plot(regret_ucb.mean(axis=0))
+        plt.ylabel('Regret')
+        plt.xlabel('t')
+        plt.show()
+
+        plt.figure(1)
+        plt.plot(rew_UCB.mean(axis=0))
+        plt.ylabel('Reward')
+        plt.xlabel('t')
+        plt.show()
+
+########################################################################################################################
+
+    def simulation_step_6(self):
+        pass
--- main.py
+++ main.py
@@ -18,6 +18,17 @@ print(experiment_1_2)
 simulator_step3 = Simulator()
 step3 = simulator_step3.simulation_step_3()
 
+
 # Step 4
 simulator_step4 = Simulator()
 step4 = simulator_step4.simulation_step_4()
+
+
+# Step 5
+simulator_step5 = Simulator()
+step5 = simulator_step5.simulation_step_5()
+
+
+# Step 6
+simulator_step6 = Simulator()
+step6 = simulator_step6.simulation_step_6()

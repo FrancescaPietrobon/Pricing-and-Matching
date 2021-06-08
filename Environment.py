Fixed Step 5-6 and added variance plot

Environment.py
@@ -53,32 +53,48 @@ class Environment_Single_Price:
 
 
 class Environment_Step5:
-    def __init__(self, margin_item2, conversion_rates_item2, daily_customers, discounts):
+    def __init__(self, margin_item1, margin_item2, conversion_rates_item1, conversion_rates_item2, daily_customers, discounts):
+        self.margin_item1 = margin_item1
         self.margin_item2 = margin_item2
+        self.conversion_rates_item1 = conversion_rates_item1
         self.conversion_rates_item2 = conversion_rates_item2
         self.daily_customers = daily_customers
         self.discounts = discounts
 
     def round(self, weights):
-        # Simulating the arrival of customers (that buy item 2)
+        buyers_item1 = np.zeros(4)
         buyers_item2 = np.zeros((4, 4))
+        offer_item1 = np.zeros(4)
         offer_item2 = np.zeros((4, 4))
         for group in range(4):
             for _ in range(self.daily_customers[group]):
-                promo = np.random.choice(4, p=weights[:, group])
-                offer_item2[promo, group] += 1
-                buyers_item2[promo, group] = buyers_item2[promo, group] + np.random.binomial(1, self.conversion_rates_item2[promo, group])
+                bin_item1 = np.random.binomial(1, self.conversion_rates_item1[group])
+                offer_item1[group] += 1
+                buyers_item1[group] = buyers_item1[group] + bin_item1
+                if bin_item1 == 1:
+                    promo = np.random.choice(4, p=weights[:, group])
+                    offer_item2[promo, group] += 1
+                    buyers_item2[promo, group] = buyers_item2[promo, group] + np.random.binomial(1, self.conversion_rates_item2[promo, group])
+
+        conversion_rates_item1_round = np.zeros(4)
+        for i in range(4):
+            conversion_rates_item1_round[i] = buyers_item1[i] / offer_item1[i] if offer_item1[i] > 0 else 0
 
         conversion_rates_item2_round = np.zeros((4, 4))
         for i in range(4):
             for j in range(4):
                 conversion_rates_item2_round[i, j] = buyers_item2[i, j] / offer_item2[i, j] if offer_item2[i, j] > 0 else 0
 
+        revenue_item1 = 0
+        revenue_item1 = revenue_item1 + buyers_item1.sum() * self.margin_item1
+
         revenue_item2 = 0
         for promo_type in range(4):
             revenue_item2 = revenue_item2 + (buyers_item2[promo_type]).sum() * self.margin_item2 * (1-self.discounts[promo_type])
 
-        return conversion_rates_item2_round, revenue_item2
+        revenue = revenue_item1 + revenue_item2
+
+        return conversion_rates_item1_round, conversion_rates_item2_round, revenue
 
 
 class Environment_Step6:

Simulator.py
@@ -11,7 +11,8 @@ from CUMSUM_UCB_Matching import CUMSUM_UCB_Matching
 from Data import *
 from Environment import *
 import matching_lp as lp
-from Matching import *
+from Learner_Conversion_Rates_Item1 import *
+from Learner_Matching import *
 from Learner_Customers import *
 from SWTS_Learner import *
 from SWTS_Learner_item2 import *
@@ -84,7 +85,7 @@ class Simulator:
 
         # Launching the experiments, using both UCB1 and Thompson Sampling to learn the price of item 1
         n_experiments = 5
-        time_horizon = 365
+        time_horizon = 1000
         ucb1_rewards_per_experiment = []
         ts_rewards_per_experiment = []
 
@@ -129,7 +130,7 @@ class Simulator:
         # Launching the experiments, using both UCB1 and Thompson Sampling to learn the price of item 1
         # This time, the number of customers and the conversion rates for item 2 are not known
         n_experiments = 5
-        time_horizon = 365
+        time_horizon = 1000
         ucb1_rewards_per_experiment = []
         ts_rewards_per_experiment = []
 
@@ -195,22 +196,20 @@ class Simulator:
 ########################################################################################################################
 
     def simulation_step_5(self, promo_fractions):
-        _, _, selected_price_item2, _ = self.simulation_step_1(promo_fractions)
+        opt, selected_price_item1, selected_price_item2, _ = self.simulation_step_1(promo_fractions)
+
+        conversion_rates_item1 = self.data.conversion_rates_item1[:, list(self.data.prices_item1).index(selected_price_item1)]
+        selected_margin_item1 = self.data.margins_item1[list(self.data.prices_item1).index(selected_price_item1)]
 
         conversion_rates_item2 = self.data.conversion_rates_item2[list(self.data.prices_item2).index(selected_price_item2)]
         selected_margin_item2 = self.data.margins_item2[list(self.data.prices_item2).index(selected_price_item2)]
-        daily_customers = self.data.daily_customers
 
-        # Objective matrix (row: promo; column: customer class)
-        daily_promos = (promo_fractions * sum(daily_customers)).astype(int)
-        opt, _ = lp.matching_lp(selected_margin_item2, self.discounts, conversion_rates_item2, daily_promos, daily_customers)
+        daily_customers = self.data.daily_customers
 
         # Launching the experiments, using UCB1 to learn the promo-customer class matching
         n_experiments = 1
-        time_horizon = 1000
-        regret_matching = np.zeros((n_experiments, time_horizon))
+        time_horizon = 5000
         reward_matching = []
-        opt_matching = []
 
         for e in range(n_experiments):
             print("Experiment ", e+1, "/", n_experiments)
@@ -219,18 +218,19 @@ class Simulator:
             env_daily_customers = Daily_Customers(mean=daily_customers, sd=25)
             learner_daily_customers = Learner_Customers(np.zeros(4))
 
+            # Learner for the mean of the conversion rates of item 1
+            learner_conversion_rates_item1 = Learner_Conversion_Rates_item1()
+
             # Learner for the mean of the conversion rates of item 2
             learner_conversion_rates_item2 = Learner_Conversion_Rates_item2()
 
             # Environment and learner for the matching between promos and customer classes
-            env = Environment_Step5(selected_margin_item2, conversion_rates_item2, daily_customers, self.discounts)
-            matching_learner = Matching(selected_margin_item2, np.full((4, 4), 0.1), daily_customers, self.discounts, promo_fractions)
+            env = Environment_Step5(selected_margin_item1, selected_margin_item2, conversion_rates_item1, conversion_rates_item2, daily_customers, self.discounts)
+            matching_learner = Matching(selected_margin_item2, np.full(4, 0.1), np.full((4, 4), 0.1), daily_customers, self.discounts, promo_fractions)
 
             rew_matching_per_exp = []
-            opt_matching_per_exp = []
 
             for t in range(time_horizon):
-                print(t)
                 # Learning the number of customers
                 daily_customers_sample = env_daily_customers.sample()
                 daily_customers_empirical_means = learner_daily_customers.update_daily_customers(daily_customers_sample)
@@ -240,27 +240,18 @@ class Simulator:
                 env.daily_customers = daily_customers_sample
 
                 # Computing the weights matrix (normalized)
-                # At the first round, we initialize the weights uniformly for each line
-                weights = matching_learner.optimize()
+                weights = matching_learner.optimize(t)
 
-                # Getting the conversion rates of item 2 from the environment
-                conversion_rates_item2_sample, revenue_item2 = env.round(weights)
+                # Getting the conversion rates from the environment and updating the learner with them
+                conversion_rates_item1_sample, conversion_rates_item2_sample, revenue = env.round(weights)
+                matching_learner.conversion_rates_item1 = learner_conversion_rates_item1.update_conversion_rates(conversion_rates_item1_sample)
                 matching_learner.conversion_rates_item2 = learner_conversion_rates_item2.update_conversion_rates(conversion_rates_item2_sample)
 
-                rew_matching_per_exp.append(revenue_item2)
+                rew_matching_per_exp.append(revenue)
 
-                daily_promos_per_round = (promo_fractions * sum(daily_customers_empirical_means)).astype(int)
-                opt_per_round, _ = lp.matching_lp(selected_margin_item2, self.discounts, conversion_rates_item2, daily_promos_per_round, daily_customers_empirical_means)
-                opt_matching_per_exp.append(opt_per_round)
-
-            regret_matching[e, :] = np.cumsum(opt_matching_per_exp) - np.cumsum(rew_matching_per_exp)
             reward_matching.append(rew_matching_per_exp)
-            opt_matching.append(opt_matching_per_exp)
 
-        #opt = [opt] * time_horizon
-        opt = np.mean(opt_matching, axis=0)
-
-        return [regret_matching, opt, reward_matching]
+        return opt, reward_matching, time_horizon
 
 ########################################################################################################################
 
@@ -276,10 +267,9 @@ class Simulator:
         opt, _, _, _ = self.simulation_step_1(promo_fractions)
 
         # Launching the experiments
-        n_experiments = 1
-        time_horizon = 5000
+        n_experiments = 3
+        time_horizon = 1000
 
-        regret_ucb = np.zeros((n_experiments, time_horizon))
         reward_ucb = []
 
         for e in range(n_experiments):
@@ -289,16 +279,14 @@ class Simulator:
             env_daily_customers = Daily_Customers(mean=daily_customers, sd=25)
             learner_daily_customers = Learner_Customers(np.zeros(4))
 
-            # Environment and learner for the price of item 1
+            # Environment and learner for the prices of the two items and the matching
             env = Environment_Step6(conversion_rates_item1, conversion_rates_item2, daily_customers, promo_fractions,
                                     margins_item1, margins_item2, self.discounts)
             ucb_learner = UCB1_items_matching(daily_customers, margins_item1, margins_item2, self.discounts, promo_fractions)
 
             rew_ucb_per_exp = []
-            opt_rew_per_exp = []
 
             for t in range(time_horizon):
-                print(t)
                 # Learning the number of customers
                 daily_customers_sample = env_daily_customers.sample()
                 daily_customers_empirical_means = learner_daily_customers.update_daily_customers(daily_customers_sample)
@@ -313,14 +301,10 @@ class Simulator:
                 ucb_learner.update(pulled_arm[0], reward)
 
                 rew_ucb_per_exp.append(reward[2])
-                opt_rew_per_exp.append(opt)
 
-            regret_ucb[e, :] = np.cumsum(opt_rew_per_exp) - np.cumsum(rew_ucb_per_exp)
             reward_ucb.append(rew_ucb_per_exp)
 
-        opt = [opt] * time_horizon
-
-        return [regret_ucb, opt, reward_ucb]
+        return opt, reward_ucb, time_horizon
 
 ########################################################################################################################
     # TODO adapt according to step 6

main.py
@@ -1,5 +1,6 @@
 from Simulator import *
 import pandas as pd
+from scipy.ndimage.filters import uniform_filter1d
 
 
 def main():
@@ -53,21 +54,22 @@ def main():
             break
 
         elif choice == 5:
-            experiment_5_1 = Simulator().simulation_step_5(promo_fractions_exp1)
-            plot_regret_matching("STEP 5 - EXP 1", experiment_5_1[0])
-            plot_reward_matching("STEP 5 - EXP 1", experiment_5_1[1], experiment_5_1[2])
-            experiment_5_2 = Simulator().simulation_step_5(promo_fractions_exp2)
-            plot_regret_matching("STEP 5 - EXP 2", experiment_5_2[0])
-            plot_reward_matching("STEP 5 - EXP 2", experiment_5_2[1], experiment_5_2[2])
+            # TODO Do not print regret since it is useless in this problem
+            opt, ucb_rew, time_horizon = Simulator().simulation_step_5(promo_fractions_exp1)
+            #plot_regret_single_learner("LP", "STEP 5 - EXP 1", opt, ucb_rew)
+            plot_reward_single_learner("LP", "STEP 5 - EXP 1", opt, ucb_rew, time_horizon)
+            opt, ucb_rew, time_horizon = Simulator().simulation_step_5(promo_fractions_exp2)
+            #plot_regret_single_learner("LP", "STEP 5 - EXP 2", opt, ucb_rew)
+            plot_reward_single_learner("LP", "STEP 5 - EXP 2", opt, ucb_rew, time_horizon)
             break
 
         elif choice == 6:
-            experiment_6_1 = Simulator().simulation_step_6(promo_fractions_exp1)
-            plot_regret_matching("STEP 6 - EXP 1", experiment_6_1[0])
-            plot_reward_matching("STEP 6 - EXP 1", experiment_6_1[1], experiment_6_1[2])
-            experiment_6_2 = Simulator().simulation_step_6(promo_fractions_exp2)
-            plot_regret_matching("STEP 6 - EXP 2", experiment_6_2[0])
-            plot_reward_matching("STEP 6 - EXP 2", experiment_6_2[1], experiment_6_2[2])
+            opt, ucb_rew, time_horizon = Simulator().simulation_step_6(promo_fractions_exp1)
+            plot_regret_single_learner("UCB1", "STEP 6 - EXP 1", opt, ucb_rew)
+            plot_reward_single_learner("UCB1", "STEP 6 - EXP 1", opt, ucb_rew, time_horizon)
+            opt, ucb_rew, time_horizon = Simulator().simulation_step_6(promo_fractions_exp2)
+            plot_regret_single_learner("UCB1", "STEP 6 - EXP 2", opt, ucb_rew)
+            plot_reward_single_learner("UCB1", "STEP 6 - EXP 2", opt, ucb_rew, time_horizon)
             break
 
         elif choice == 7:
@@ -90,18 +92,25 @@ def plot_regret(step, opt, ucb1_rewards_per_exp, ts_rewards_per_exp):
     plt.figure()
     plt.xlabel("t")
     plt.ylabel("Regret")
-    #x = np.arange(len(ucb1_rewards_per_exp[0]), dtype=float)
+    x = np.arange(len(ucb1_rewards_per_exp[0]), dtype=float)
 
     y_ucb = np.cumsum(np.mean(opt - ucb1_rewards_per_exp, axis=0))
-    #conf_int_ucb = 1.960 * np.std(y_ucb) / np.mean(y_ucb)
+    y_ucb_std = np.std(opt - ucb1_rewards_per_exp, axis=0)
     plt.plot(y_ucb, "b")
-    #plt.fill_between(x, (y_ucb - conf_int_ucb), (y_ucb + conf_int_ucb), color='b', alpha=.3)
+    plt.fill(np.concatenate([x, x[::-1]]),
+             np.concatenate([uniform_filter1d(y_ucb - 60 * y_ucb_std, size=30),
+                             uniform_filter1d((y_ucb + 60 * y_ucb_std)[::-1], size=30)]),
+             alpha=.3, fc='b')
 
     y_ts = np.cumsum(np.mean(opt - ts_rewards_per_exp, axis=0))
-    #conf_int_ts = 1.960 * np.std(y_ts) / np.mean(y_ts)
+    y_ts_std = np.std(ts_rewards_per_exp, axis=0)
     plt.plot(y_ts, "r")
-    #plt.fill_between(x, (y_ts - conf_int_ts), (y_ts + conf_int_ts), color='r', alpha=.3)
+    plt.fill(np.concatenate([x, x[::-1]]),
+             np.concatenate([uniform_filter1d(y_ts - 60 * y_ts_std, size=30),
+                             uniform_filter1d((y_ts + 60 * y_ts_std)[::-1], size=30)]),
+             alpha=.3, fc='r')
 
+    plt.ylim(bottom=0.)
     plt.legend(["UCB1", "TS"], title=step)
     plt.show()
 
@@ -118,22 +127,33 @@ def plot_reward(step, opt, ucb1_rewards_per_exp, ts_rewards_per_exp, time_horizo
     plt.show()
 
 
-def plot_regret_matching(step, ucb_matching_regret):
-    plt.figure(0)
-    plt.xlabel('t')
-    plt.ylabel('Regret')
-    plt.plot(ucb_matching_regret.mean(axis=0), "b")
-    plt.title(step)
+def plot_regret_single_learner(learner, step, opt, ucb1_rewards_per_exp):
+    plt.figure()
+    plt.xlabel("t")
+    plt.ylabel("Regret")
+    x = np.arange(len(ucb1_rewards_per_exp[0]), dtype=float)
+
+    y_ucb = np.cumsum(np.mean(opt - ucb1_rewards_per_exp, axis=0))
+    y_ucb_std = np.std(opt - ucb1_rewards_per_exp, axis=0)
+    plt.plot(y_ucb, "b")
+    plt.fill(np.concatenate([x, x[::-1]]),
+             np.concatenate([uniform_filter1d(y_ucb - 60 * y_ucb_std, size=30),
+                             uniform_filter1d((y_ucb + 60 * y_ucb_std)[::-1], size=30)]),
+             alpha=.3, fc='b')
+
+    plt.ylim(bottom=0.)
+    plt.legend([learner], title=step)
     plt.show()
 
 
-def plot_reward_matching(step, opt, ucb_matching_reward):
-    plt.figure(1)
-    plt.xlabel('t')
-    plt.ylabel('Reward')
-    plt.plot(np.mean(ucb_matching_reward, axis=0), "b")
+def plot_reward_single_learner(learner, step, opt, ucb1_rewards_per_exp, time_horizon):
+    plt.figure()
+    plt.xlabel("t")
+    plt.ylabel("Reward")
+    plt.plot(np.mean(ucb1_rewards_per_exp, axis=0), "b")
+    opt = [opt] * time_horizon
     plt.plot(opt, '--k')
-    plt.legend(["UCB1", "OPTIMAL"], title=step)
+    plt.legend([learner, "OPTIMAL"], title=step)
     plt.show()
 
 

matching_lp.py
@@ -1,25 +1,27 @@
 from scipy.optimize import linprog
+import warnings
+warnings.filterwarnings(action='ignore', module='scipy')
 
 
 def matching_lp(margin_item2, discounts, conversion_rates_item2, daily_promos, daily_customers):
 
     # Objective Function
-    c = [margin_item2 * conversion_rates_item2[0][0] * (1 - discounts[0]),  # P0 - Class1
-         margin_item2 * conversion_rates_item2[0][1] * (1 - discounts[0]),  # P0 - Class2
-         margin_item2 * conversion_rates_item2[0][2] * (1 - discounts[0]),  # P0 - Class3
-         margin_item2 * conversion_rates_item2[0][3] * (1 - discounts[0]),  # P0 - Class4
-         margin_item2 * conversion_rates_item2[1][0] * (1 - discounts[1]),  # P1 - Class1
-         margin_item2 * conversion_rates_item2[1][1] * (1 - discounts[1]),  # P1 - Class2
-         margin_item2 * conversion_rates_item2[1][2] * (1 - discounts[1]),  # P1 - Class3
-         margin_item2 * conversion_rates_item2[1][3] * (1 - discounts[1]),  # P1 - Class4
-         margin_item2 * conversion_rates_item2[2][0] * (1 - discounts[2]),  # P2 - Class1
-         margin_item2 * conversion_rates_item2[2][1] * (1 - discounts[2]),  # P2 - Class2
-         margin_item2 * conversion_rates_item2[2][2] * (1 - discounts[2]),  # P2 - Class3
-         margin_item2 * conversion_rates_item2[2][3] * (1 - discounts[2]),  # P2 - Class4
-         margin_item2 * conversion_rates_item2[3][0] * (1 - discounts[3]),  # P3 - Class1
-         margin_item2 * conversion_rates_item2[3][1] * (1 - discounts[3]),  # P3 - Class2
-         margin_item2 * conversion_rates_item2[3][2] * (1 - discounts[3]),  # P3 - Class3
-         margin_item2 * conversion_rates_item2[3][3] * (1 - discounts[3])]            # P3 - Class4
+    c = [margin_item2 * conversion_rates_item2[0][0] * (1 - discounts[0]),          # P0 - Class1
+         margin_item2 * conversion_rates_item2[0][1] * (1 - discounts[0]),          # P0 - Class2
+         margin_item2 * conversion_rates_item2[0][2] * (1 - discounts[0]),          # P0 - Class3
+         margin_item2 * conversion_rates_item2[0][3] * (1 - discounts[0]),          # P0 - Class4
+         margin_item2 * conversion_rates_item2[1][0] * (1 - discounts[1]),          # P1 - Class1
+         margin_item2 * conversion_rates_item2[1][1] * (1 - discounts[1]),          # P1 - Class2
+         margin_item2 * conversion_rates_item2[1][2] * (1 - discounts[1]),          # P1 - Class3
+         margin_item2 * conversion_rates_item2[1][3] * (1 - discounts[1]),          # P1 - Class4
+         margin_item2 * conversion_rates_item2[2][0] * (1 - discounts[2]),          # P2 - Class1
+         margin_item2 * conversion_rates_item2[2][1] * (1 - discounts[2]),          # P2 - Class2
+         margin_item2 * conversion_rates_item2[2][2] * (1 - discounts[2]),          # P2 - Class3
+         margin_item2 * conversion_rates_item2[2][3] * (1 - discounts[2]),          # P2 - Class4
+         margin_item2 * conversion_rates_item2[3][0] * (1 - discounts[3]),          # P3 - Class1
+         margin_item2 * conversion_rates_item2[3][1] * (1 - discounts[3]),          # P3 - Class2
+         margin_item2 * conversion_rates_item2[3][2] * (1 - discounts[3]),          # P3 - Class3
+         margin_item2 * conversion_rates_item2[3][3] * (1 - discounts[3])]          # P3 - Class4
 
     # Inequality constraints: the sum of promo codes given to a class does not exceed the customers of that class
     a_inequality = [[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],       # Class1
@@ -41,9 +43,10 @@ def matching_lp(margin_item2, discounts, conversion_rates_item2, daily_promos, d
     bounds = [(0, None)] * 16
 
     # Optimization
-    res = linprog(c, a_inequality, b_inequality, a_equality, b_equality, bounds)
+    options = dict(cholesky=False, sym_pos=False, lstsq=True, presolve=True)
+    result = linprog(c, a_inequality, b_inequality, a_equality, b_equality, bounds, options=options)
 
     # Reshaping and casting the variables values found
-    matching_matrix = res.x.reshape((4, 4)).astype(int)
+    matching_matrix = result.x.reshape((4, 4)).astype(int)
 
-    return res.fun, matching_matrix
+    return result.fun, matching_matrix
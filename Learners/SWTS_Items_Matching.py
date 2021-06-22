0d2cb1d5530ae1813c511b2b07053b9de9c1fd03
Francesca Pietrobon <francesca13pietrobon@gmail.com>
2021-06-22 22:16:41 +0200

    Fixed Step 7

--- Learners/SWTS_Items_Matching.py
+++ Learners/SWTS_Items_Matching.py
@@ -13,9 +13,9 @@ class SWTS_Items_Matching():
         self.rewards_per_arm_item2 = [[[[] for _ in range(4)] for _ in range(4)] for _ in range(len(margins_item2))]
 
         self.margins = list(itertools.product(margins_item1, margins_item2))
-        self.empirical_means_item1 = np.zeros((len(margins_item1), 4))
+        #self.empirical_means_item1 = np.zeros((len(margins_item1), 4))
         self.beta_parameters_item1 = np.ones((len(margins_item1), 4, 2))
-        self.empirical_means_item2 = np.zeros((len(margins_item2), 4, 4))
+        #self.empirical_means_item2 = np.zeros((len(margins_item2), 4, 4))
         self.beta_parameters_item2 = np.ones((len(margins_item2), 4, 4, 2))
 
         self.margins_item1 = margins_item1
@@ -25,27 +25,21 @@ class SWTS_Items_Matching():
         self.promo_fractions = promo_fractions
 
     def pull_arm(self):
-        if self.t < len(self.margins):
-            arm = np.unravel_index(self.t, (len(self.margins_item1), len(self.margins_item2)))
-            matching = np.random.rand(4, 4)
-        else:
-            revenue = np.zeros((len(self.margins_item1), len(self.margins_item2)))
-            for margin1 in range(len(self.margins_item1)):
-                for margin2 in range(len(self.margins_item2)):
-                    daily_promos = (self.promo_fractions * sum(self.daily_customers * (self.empirical_means_item1[margin1]))).astype(int)
-                    revenue[margin1][margin2] = self.margins_item1[margin1] * (self.daily_customers * (self.empirical_means_item1[margin1])).sum() +\
-                                                   lp.matching_lp(self.margins_item2[margin2], self.discounts, (self.empirical_means_item2[margin2]),
-                                                                  daily_promos, (self.daily_customers * (self.empirical_means_item1[margin1])).astype(int))[0]
+        value = np.zeros((len(self.margins_item1), len(self.margins_item2)))
+        matching = np.zeros((len(self.margins_item1), len(self.margins_item2), 4, 4))
 
-            arm_flat = np.argmax(np.random.random(revenue.shape) * (revenue == np.amax(revenue, None, keepdims=True)), None)
-            arm = np.unravel_index(arm_flat, revenue.shape)
+        for margin1 in range(len(self.margins_item1)):
+            beta_item1 = np.random.beta(self.beta_parameters_item1[margin1, :, 0], self.beta_parameters_item1[margin1, :, 1])
+            for margin2 in range(len(self.margins_item2)):
+                beta_item2 = np.random.beta(self.beta_parameters_item2[margin2, :, :, 0], self.beta_parameters_item2[margin2, :, :, 1])
+                daily_promos = (self.promo_fractions * sum(self.daily_customers * np.dot(self.daily_customers, beta_item1))).astype(int)
+                reward_item2, matching[margin1][margin2] = lp.matching_lp(self.margins_item2[margin2], self.discounts, beta_item2, daily_promos, (self.daily_customers * beta_item1).astype(int))
+                value[margin1][margin2] = self.margins_item1[margin1] * (self.daily_customers * beta_item1).sum() + reward_item2
 
-            selected_margin_item2 = self.margins_item2[arm[1]]
-            daily_promos = (self.promo_fractions * sum(self.daily_customers * (self.empirical_means_item1[arm[0]]))).astype(int)
-            _, matching = lp.matching_lp(selected_margin_item2, self.discounts, (self.empirical_means_item2[arm[1]]),
-                                         daily_promos, (self.daily_customers * (self.empirical_means_item1[arm[0]])).astype(int))
+        arm_flat = np.argmax(np.random.random(value.shape) * (value == np.amax(value, None, keepdims=True)), None)
+        arm = np.unravel_index(arm_flat, value.shape)
 
-        return arm, matching
+        return arm, matching[arm[0]][arm[1]]
 
     def update(self, pulled_arm, reward):
         self.t += 1
@@ -57,12 +51,16 @@ class SWTS_Items_Matching():
             n_rounds_arm_item1 = len(self.rewards_per_arm_item1[pulled_arm[0]][class_type][-self.window_size:])
             self.beta_parameters_item1[pulled_arm[0], class_type, 0] = cum_rew_item1 + 1.0
             self.beta_parameters_item1[pulled_arm[0], class_type, 1] = n_rounds_arm_item1 - cum_rew_item1 + 1.0
-            #sample = np.random.beta(self.beta_parameters_item1[pulled_arm[0], class_type, 0],
-            #                        self.beta_parameters_item1[pulled_arm[0], class_type, 1])
-            #self.empirical_means_item1[pulled_arm[0], class_type] = (self.empirical_means_item1[pulled_arm[0], class_type] *
-            #                                                         (self.t - 1) + sample) / self.t
+            '''
+            self.empirical_means_item1[pulled_arm[0], class_type] = np.random.beta(self.beta_parameters_item1[pulled_arm[0], class_type, 0],
+                                   self.beta_parameters_item1[pulled_arm[0], class_type, 1])
+            sample = np.random.beta(self.beta_parameters_item1[pulled_arm[0], class_type, 0],
+                                    self.beta_parameters_item1[pulled_arm[0], class_type, 1])
+            self.empirical_means_item1[pulled_arm[0], class_type] = (self.empirical_means_item1[pulled_arm[0], class_type] *
+                                                                     (self.t - 1) + sample) / self.t
             self.empirical_means_item1[pulled_arm[0], class_type] = self.beta_parameters_item1[pulled_arm[0], class_type, 0] /\
                                                                     (self.beta_parameters_item1[pulled_arm[0], class_type, 0] + self.beta_parameters_item1[pulled_arm[0], class_type, 1])
+            '''
 
         # Item 2
         for promo_type in range(4):
@@ -72,10 +70,14 @@ class SWTS_Items_Matching():
                 n_rounds_arm_item2 = len(self.rewards_per_arm_item2[pulled_arm[1]][promo_type][class_type][-self.window_size:])
                 self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 0] = cum_rew_item2 + 1.0
                 self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 1] = n_rounds_arm_item2 - cum_rew_item2 + 1.0
-                #sample = np.random.beta(self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 0],
-                #                        self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 1])
-                #self.empirical_means_item2[pulled_arm[1], promo_type, class_type] = (self.empirical_means_item2[pulled_arm[1], promo_type, class_type] *
-                #                                                                     (self.t - 1) + sample) / self.t
+                '''
+                self.empirical_means_item2[pulled_arm[1], promo_type, class_type] = np.random.beta(self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 0],
+                                        self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 1])
+                sample = np.random.beta(self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 0],
+                                        self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 1])
+                self.empirical_means_item2[pulled_arm[1], promo_type, class_type] = (self.empirical_means_item2[pulled_arm[1], promo_type, class_type] *
+                                                                                     (self.t - 1) + sample) / self.t
                 self.empirical_means_item2[pulled_arm[1], promo_type, class_type] = self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 0] / \
                                                                         (self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 0] + self.beta_parameters_item2[pulled_arm[1], promo_type, class_type, 1])
+                '''
 
